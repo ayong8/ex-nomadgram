6ac1ceeda920f71cc527b675cab22efd74f167c2
ayong8 <yong8@me.com>
2018-02-14 21:56:41 -0500

    django updated

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -4,16 +4,28 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
+# Create your models here.
 
-@python_2_unicode_compatible
 class User(AbstractUser):
+	""" User model """
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
 
-    def __str__(self):
-        return self.username
+	GENDER_CHOICES = (
+		('male', 'Male'),
+		('female', 'Female'),
+		('not-specified', 'Not specified')
+	)
 
-    def get_absolute_url(self):
-        return reverse('users:detail', kwargs={'username': self.username})
+	name = models.CharField(_('Name of User'), blank=True, max_length=255)
+	website = models.URLField(null=True) # null=True... 디폴트값을 남기기 위함
+	bio = models.TextField(null=True)
+	phone = models.CharField(max_length=140, null=True)
+	gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+	followers = models.ManyToManyField("self")	# user to user이므로 해당 클래스 user을 참조
+	following = models.ManyToManyField("self")
+
+	def __str__(self):
+		return self.username
+
+	def get_absolute_url(self):
+		return reverse('users:detail', kwargs={'username': self.username})
