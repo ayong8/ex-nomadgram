d9a84a7536c724c57dcc89d57bb2cf56e05eedb4
ayong8 <yong8@me.com>
2018-02-16 00:00:40 -0500

    added admin components

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.coding import python_2_unicode_compatible
+from django.utils.encoding import python_2_unicode_compatible
 from nomadgram.users import models as user_models
 
 class TimeStampedModel(models.Model):
