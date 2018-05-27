dd84849d0d5a38ff398f8fc0b5c3622bae266f36
ayong8 <yong8@me.com>
2018-05-26 23:44:08 -0400

    ...

--- nomadgram/images/apps.py
+++ nomadgram/images/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class ImagesConfig(AppConfig):
     name = 'images'
+
+    verbose_name = "Users"
+
+    def ready(self):
+        """Override this to put in:
+            Users system checks
+            Users signal registration
+        """
+        pass
--- nomadgram/images/urls.py
+++ nomadgram/images/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(
+        regex=r'all/$',
+        view=views.ListAllImages.as_view(),
+        name='all_images'
+    ),
     url(
         regex=r'all/$',
         view=views.ListAllImages.as_view(),
